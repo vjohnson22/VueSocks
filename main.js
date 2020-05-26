6ffc45e2ecebbe74fa0f2b87e7efd126a8ced327
Victor Johnson <victor@thinknimble.com>
2020-05-26 14:55:19 -0400

    first two lessons

---
+++ main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    description: 'These are my new socks',
+    image: './assets/images/socks.jpg',
+    link: 'http://www.google.com',
+  },
+})
